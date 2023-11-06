ea8eb323e68bdf34322db65ba698bc64668ab82f
chae1xx1os <kty1895@naver.com>
2023-11-07 01:35:01 +0900

    [6-14] Detail info

--- screens/detail_screen.dart
+++ screens/detail_screen.dart
@@ -5,14 +5,12 @@ import 'package:flutter_application_1/services/api_service.dart';
 
 class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
-
   const DetailScreen({
     super.key,
     required this.title,
     required this.thumb,
     required this.id,
   });
-
   @override
   State<DetailScreen> createState() => _DetailScreenState();
 }
@@ -20,7 +18,6 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   late Future<WebtoonDetailModel> webtoon;
   late Future<List<WebtoonEpisodeModel>> episodes;
-
   @override
   void initState() {
     super.initState();
@@ -71,6 +68,38 @@ class _DetailScreenState extends State<DetailScreen> {
               ),
             ],
           ),
+          const SizedBox(
+            height: 25,
+          ),
+          FutureBuilder(
+            future: webtoon,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 50,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        snapshot.data!.about,
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Text(
+                        '${snapshot.data!.genre} / ${snapshot.data!.age}',
+                        style: const TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                );
+              }
+              return const Text("...");
+            },
+          )
         ],
       ),
     );
