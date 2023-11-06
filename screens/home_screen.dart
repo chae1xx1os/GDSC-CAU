6c2b2f048a908c6b253d3daf394334d9fbaf7410
chae1xx1os <kty1895@naver.com>
2023-11-07 01:05:23 +0900

    [6-7] ListView

--- screens/home_screen.dart
+++ screens/home_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter_application_1/services/api_service.dart';
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
 
-  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +16,7 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
         title: const Text(
-          "오늘의 웹툰",
+          "어늘의 웹툰",
           style: TextStyle(
             fontSize: 24,
           ),
@@ -26,9 +26,19 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return const Text("There is data!");
+            return ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                var webtoon = snapshot.data![index];
+                return Text(webtoon.title);
+              },
+              separatorBuilder: (context, index) => const SizedBox(width: 20),
+            );
           }
-          return const Text('Loading....');
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
         },
       ),
     );
